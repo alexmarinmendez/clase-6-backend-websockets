375ba40f56b057f5b51263a101528d0f94774cd4
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-04-15 11:24:03 -0500

    feat: add emit in two way direcction

--- app.js
+++ app.js
@@ -2,12 +2,19 @@ const express = require('express')
 const { Server } = require('socket.io')
 
 const app = express()
-const serverHttp = app.listen(8080, () => console.log('Server Up'))
+const httpServer = app.listen(8080, () => console.log('Server Up'))
 
 app.use(express.static('public'))
 
-const serverSocket = new Server(serverHttp)
+const socketServer = new Server(httpServer)
 
-serverSocket.on('connection', () => {
+let log = []
+
+socketServer.on('connection', (socketClient) => {
     console.log('Nuevo cliente conectado...')
+    socketClient.on('message', (data) => {
+        console.log(`Me enviaron: ${data}`)
+        log.push(data)
+        socketClient.emit('history', log)
+    })
 })
--- public/index.js
+++ public/index.js
@@ -1,6 +1,18 @@
-const socket = io()
+const socketClient = io()
 let chatBox = document.getElementById('chatBox')
 
 chatBox.addEventListener('keyup', evt => {
-    console.log(evt.key)
+    if (evt.key == "Enter")  {
+        socketClient.emit('message', chatBox.value)
+        chatBox.value = ""
+    }
+})
+
+socketClient.on('history', data => {
+    let history = document.getElementById('history')
+    let messages = ''
+    data.forEach(item => {
+        messages+= `${item}<br />`
+    })
+    history.innerHTML = messages
 })
