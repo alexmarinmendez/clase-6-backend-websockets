de4b0b627b3cbaa0474c1d2f3d16a005b56496c3
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-04-15 12:30:33 -0500

    feat: add handlebars settings

--- src/app.js
+++ src/app.js
@@ -1,12 +1,19 @@
 const express = require('express')
+const handlebars = require('express-handlebars')
 const { Server } = require('socket.io')
+const routerViews = require('./routes/views.router.js')
 
 const app = express()
 const httpServer = app.listen(8080, () => console.log('Server Up'))
-
-app.use(express.static('public'))
-
 const socketServer = new Server(httpServer)
+
+app.engine('handlebars', handlebars.engine())
+app.set('views', __dirname+'/views')
+// app.set('views','./src/views')
+app.set('view engine', 'handlebars')
+app.use(express.static(__dirname+'/public'))
+app.use('/', routerViews)
+
 
 let log = []
 
--- src/public/index.js
+++ src/public/index.js
@@ -1,18 +1,20 @@
-const socketClient = io()
-let chatBox = document.getElementById('chatBox')
+// const socketClient = io()
+// let chatBox = document.getElementById('chatBox')
 
-chatBox.addEventListener('keyup', evt => {
-    if (evt.key == "Enter")  {
-        socketClient.emit('message', chatBox.value)
-        chatBox.value = ""
-    }
-})
+// chatBox.addEventListener('keyup', evt => {
+//     if (evt.key == "Enter")  {
+//         socketClient.emit('message', chatBox.value)
+//         chatBox.value = ""
+//     }
+// })
 
-socketClient.on('history', data => {
-    let history = document.getElementById('history')
-    let messages = ''
-    data.forEach(item => {
-        messages+= `[${item.userId}] dice: ${item.message}<br />`
-    })
-    history.innerHTML = messages
-})
+// socketClient.on('history', data => {
+//     let history = document.getElementById('history')
+//     let messages = ''
+//     data.forEach(item => {
+//         messages+= `[${item.userId}] dice: ${item.message}<br />`
+//     })
+//     history.innerHTML = messages
+// })
+
+console.log('Hola INDEX')
